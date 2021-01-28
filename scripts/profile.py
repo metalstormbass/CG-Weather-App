f78c0702e7b403c36e545d272e474a09423e354d
amit-schnitzer <amit.schnitzer@gmail.com>
2021-01-28 09:40:46 +0200

    modified profile.py code a little

--- scripts/profile.py
+++ scripts/profile.py
@@ -1,6 +1,7 @@
 import requests
 from threading import Thread
 import time
+import sys
 
 #remove https warning
 requests.packages.urllib3.disable_warnings()
@@ -15,14 +16,16 @@ def post(target, city, api_key):
        time.sleep(0.05)
        output = response.content
        output = output.decode('utf-8')
-       print (".")
+       print (">", end="")
+       sys.stdout.flush()
 
 #Input target
 print("Weather App - Lambda Function")
 target = input("Target: ")
 city = input("City: ")
 api_key =  input("API Key: ")
-print ("Working ")
+print ("Working ", end="-")
+
 for x in range(0, 200):
   t = Thread(target=post, args=(target, city, api_key)) 
   t.start()  
